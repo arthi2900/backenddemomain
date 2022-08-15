a9f3983b0028aa3ad69f53d13b6d675632639b3a
arthi2900 <aaarthi2927@gmail.com>
2022-08-15 15:59:10 +0530

    add

--- index.js
+++ index.js
@@ -1,28 +1,171 @@
-import express from "express";
-import {MongoClient} from"mongodb";
-import cors from"cors";
-import dotenv from "dotenv";
-import {AuthRouter} from "./AuthRouter.js";
-import { UserRouter } from "./User.js";
-dotenv.config();
-const Mongo_url=process.env.Mongo_url;
-const PORT=process.env.PORT || 8000;
-export const app=express();
-app.use (express.json());
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+const mongodb = require("mongodb");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const mongoClient = mongodb.MongoClient;
+const URL =
+  "mongodb+srv://shan1:shan1@cluster0.9trsz.mongodb.net/?retryWrites=true&w=majority";
+
 app.use(
-    cors({
-      origin: "*",
-    })
-  );
-async function createConnection(){
-    const client=new MongoClient(Mongo_url);
-    await client.connect();
-    console.log("I connect successfull");
-    return client;
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+
+//authenticate
+function authenticate(req, res, next) {
+  if (req.headers.authorization) {
+    let decode = jwt.verify(req.headers.authorization, "thisisasecretkey");
+    if (decode) {
+      req.userId = decode.id;
+      next();
+    } else {
+      res.status(401).json({ message: "Unauthorized1" });
     }
-    export const client=await createConnection();
-    app.use("/auth",AuthRouter);
-    app.use("/user",UserRouter);
-        app.listen(PORT,function(){
-        console.log(`successful start from connect ${PORT}`);
-    })
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+}
+
+app.get("/viewrepo", authenticate, async (req, res) => {
+  try {
+    let connection = await mongoClient.connect(URL);
+    let db = connection.db("data");
+    let students = await db
+      .collection("details")
+      .find({ createdBy: req.userId })
+      .toArray();
+    await connection.close();
+
+    res.json(students);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.post("/newrepo", authenticate, async (req, res) => {
+  try {
+    let connection = await mongoClient.connect(URL);
+
+    let db = connection.db("data");
+    req.body.createdBy = req.userId;
+    await db.collection("details").insertOne(req.body);
+
+    await connection.close();
+
+    res.json({ message: "Student Added" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.put("/editrepo/:id", async (req, res) => {
+  try {
+    let connection = await mongoClient.connect(URL);
+
+    let db = connection.db("data");
+
+    await db
+      .collection("details")
+      .updateOne({ _id: mongodb.ObjectId(req.params.id) }, { $set: req.body });
+
+    await connection.close();
+
+    res.json({ message: "Student Updated" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.delete("/deleterepo/:id", authenticate, async (req, res) => {
+  try {
+    let connection = await mongoClient.connect(URL);
+
+    let db = connection.db("data");
+
+    await db
+      .collection("details")
+      .deleteOne({ _id: mongodb.ObjectId(req.params.id) });
+
+    await connection.close();
+
+    res.json({ message: "Student Deteleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.get("/repo/:id", authenticate, async (req, res) => {
+  try {
+    let connection = await mongoClient.connect(URL);
+
+    let db = connection.db("data");
+
+    let student = await db
+      .collection("details")
+      .findOne({ _id: mongodb.ObjectId(req.params.id) });
+
+    await connection.close();
+
+    res.json(student);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    let connection = await mongoClient.connect(URL);
+    let db = (await connection).db("data");
+
+    let salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(req.body.password, salt);
+    req.body.password = hash;
+
+    await db.collection("users").insertOne(req.body);
+    await connection.close();
+    res.json({ message: "User Created" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    let connection = await mongoClient.connect(URL);
+    let db = connection.db("data");
+    let user = await db.collection("users").findOne({ email: req.body.email });
+    if (user) {
+      let compare = bcrypt.compareSync(req.body.password, user.password);
+      if (compare) {
+        let token = jwt.sign(
+          { name: user.name, id: user._id },
+          "thisisasecretkey",
+          { expiresIn: "1h" }
+        );
+        res.json({ token });
+      } else {
+        res.status(500).json({ message: "Credientials does not match" });
+      }
+    } else {
+      res.status(401).json({ message: "Credientials does not match" });
+    }
+    await connection.close();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log("Web server Started");
+});
