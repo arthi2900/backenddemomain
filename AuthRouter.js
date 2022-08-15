cce809f04959d06b13be5ff445c07f0ac7df391b
arthi2900 <aaarthi2927@gmail.com>
2022-08-15 14:09:59 +0530

    add

--- AuthRouter.js
+++ AuthRouter.js
@@ -38,36 +38,12 @@ if(isPasswordMatch){
 const token=jwt.sign({id:userfromdb._id},process.env.SECRET_KEY);
 const user=username;
 res.send({message:"successful login",token:token,user:user});
-
+res.send(userstoretoken);
 }
 else{
 res.status(401).send({message:"Invalid credenitials"});
 }
    }
  })
- /*
-router.get("/user", async function (req, res) {
-    const result =await client.db("Todo").collection("user")
-    .find({}).toArray();
-    res.send(result);
-
-});
-
-router.get("/:id",async function (req, res) {
-    const {id}=req.params;
-    const result =await client.db("Todo").collection("user")
-    .findOne({_id:ObjectId(id)});
-    res.send(result);
-
-});
-router.delete("/:id",async function  (req, res) {
-   const {id}=req.params;
-   const result =await client.db("Todo").collection("user")
-   .deleteOne({_id:ObjectId(id)});
-   res.send(result);
-  
-    
-    
-});
-*/
+ 
 export const AuthRouter=router;
