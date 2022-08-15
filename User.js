49cc18a1c4aa642161cd0bee3f55c8637e771967
arthi2900 <aaarthi2927@gmail.com>
2022-08-15 19:35:05 +0530

    add

--- User.js
+++ User.js
@@ -13,7 +13,7 @@ router.get("/",auth,async function(req,res){
    //const =req.params;
       const result =await client.db("Todo").collection("user")
         .findOne({});
-      res.status(200).json({username:result.username});
+      res.status(200).json({username:result.username,id:result._id});
     //res.send(re);
 })
 
