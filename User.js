7affca8a14369e33992739053199bfd3c88a58c0
arthi2900 <aaarthi2927@gmail.com>
2022-08-15 19:38:17 +0530

    add

--- User.js
+++ User.js
@@ -3,12 +3,13 @@ const router=express.Router();
 import {client} from "./index.js";
 import { ObjectId } from "mongodb";
 import {auth} from "./auth.js";
-/*router.get("/",auth,async function(req,res){
-    const result =await client.db("Todo").collection("user")
-    .find({}).toArray();
+router.get("/:id",auth,async function(req,res){
+  const id =req.params; 
+  const result =await client.db("Todo").collection("user")
+  .findOne({_id:ObjectId(id)});
     res.send(result);
 })
-*/
+
 router.get("/",auth,async function(req,res){
    //const =req.params;
       const result =await client.db("Todo").collection("user")
